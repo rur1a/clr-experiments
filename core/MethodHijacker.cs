a526b0776ff1afb276044d058cfab4e801fa1777
rur1a <rubishiner@gmail.com>
2024-07-17 19:51:18 +0200

    override method in sealed class

---
+++ core/MethodHijacker.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+public class MethodHijacker : IMethodHijacker
+{
+    public unsafe void HijackMethod(MethodInfo source, MethodInfo destination)
+    {
+        RuntimeHelpers.PrepareMethod(source.MethodHandle);
+        RuntimeHelpers.PrepareMethod(destination.MethodHandle);
+        
+        var sourceFp = source.MethodHandle.GetFunctionPointer();
+        var destinationFp = destination.MethodHandle.GetFunctionPointer();
+        
+        var sourcePointer = (byte*)sourceFp.ToPointer();
+        var destinationPointer = (byte*)destinationFp.ToPointer();
+
+        VirtualProtect(sourcePointer, 8, 0x40, out var oldProtect);
+        
+        var jump = (uint)destinationPointer - (uint)sourcePointer - 5;
+        *sourcePointer = 0xE9;
+        *(uint*)(sourcePointer + 1) = jump;
+
+        VirtualProtect(sourcePointer, 8, oldProtect, out oldProtect);
+    }
+
+    [DllImport("kernel32")]
+    static extern unsafe bool VirtualProtect(byte* lpAddress, uint dwSize, uint flNewProtect, out uint lpflOldProtect);
+}
+
+public interface IMethodHijacker
+{
+    void HijackMethod(MethodInfo source, MethodInfo destination);
+}
--- core/Program.cs
+++ core/Program.cs
@@ -1,21 +1,18 @@
-﻿using var fileWriteStream = File.OpenWrite("data.bin");
-var data = new[]
-{
-    new SampleStruct { B1 = 0xAA, B2 = 0xBB, I = 0xCCDDEEFF, L = 0x001122334455667788U },
-    new SampleStruct { B1 = 0xAA, B2 = 0xBB, I = 0xCCDDEEFF, L = 0x001122334455667788U },
-    new SampleStruct { B1 = 0xAA, B2 = 0xBB, I = 0xCCDDEEFF, L = 0x001122334455667788U },
-};
-UnsafeSerializer.Serialize(fileWriteStream, data);
+﻿var source = typeof(OriginClass).GetMethod("Method");
+var destination = typeof(HijackedClass).GetMethod("Method");
+    
+var hijacker = new MethodHijacker();
+hijacker.HijackMethod(source, destination);
 
-using var fileReadStream = File.OpenRead("data.bin");
-var deserializedData = UnsafeSerializer.Deserialize<SampleStruct>(fileReadStream); 
-foreach (var item in deserializedData) 
-    Console.WriteLine($"B1: {item.B1:X2}, B2: {item.B2:X2}, I: {item.I:X8}, L: {item.L:X16}");
+var originClass = new OriginClass();
+originClass.Method();
 
-struct SampleStruct
+sealed class OriginClass
 {
-    public byte B1;
-    public byte B2;
-    public ulong L;
-    public uint I;
+    public void Method() => Console.WriteLine("Origin Method");
+}
+
+sealed class HijackedClass
+{
+    public void Method() => Console.WriteLine("Hijacked Method");
 }
